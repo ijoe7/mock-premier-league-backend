c830cafd51375ae93f8f75c972e99b0c9247a824
Segun <iyandaoluwasegun7@gmail.com>
2022-02-22 13:20:46 +0100

    docker link updated on README

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -7,6 +7,11 @@ import { createClient } from 'redis';
 let client = createClient({
   url: process.env.REDIS_URL
 });
+if (!process.env.REDIS_URL) {
+  client = createClient({
+    url: "redis://:pa6881ac048779148a15338d77bc3a29e38f124ac415b2edcc36e3922e1c56b92@ec2-34-206-217-83.compute-1.amazonaws.com:7879",
+  });
+}
 // if (process.env.REDISTOGO_URL) {
 //   let redisURL: any = process.env.REDISTOGO_URL;
 //   client = createClient(redisURL);
